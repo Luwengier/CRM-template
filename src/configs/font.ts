3e7339b1f9db3d7aa4a852319f0cdf19be388c59
Luewngier <g217881111@gmail.com>
2023-06-15 16:25:17 +0800

    build: add font setting

---
+++ src/configs/font.ts
@@ -0,0 +1,7 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import '@fontsource/noto-sans-tc/300.css'
+import '@fontsource/noto-sans-tc/500.css'
+import '@fontsource/noto-sans-tc/700.css'
