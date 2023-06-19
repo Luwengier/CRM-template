9164b37e0570f40b970fee9a0765be30d03344f9
Luewngier <g217881111@gmail.com>
2023-06-19 16:05:51 +0800

    build: create NotistackProvider in providers

---
+++ src/providers/NotistackProvider.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react'
+import { SnackbarProvider } from 'notistack'
+import { NotifyAlert } from 'components/Alert'
+
+// Type
+interface Props {
+  children: ReactNode
+}
+
+type ExtraProps = {
+  actions?: {
+    label: string
+    action: () => void
+  }[]
+}
+
+declare module 'notistack' {
+  interface VariantOverrides {
+    error: ExtraProps
+    warning: ExtraProps
+    default: ExtraProps
+    success: ExtraProps
+    info: ExtraProps
+  }
+}
+
+// Provider
+export const NotistackProvider = ({ children }: Props) => {
+  return (
+    <SnackbarProvider
+      maxSnack={5}
+      preventDuplicate
+      autoHideDuration={4000}
+      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      Components={{
+        error: NotifyAlert,
+        warning: NotifyAlert,
+        default: NotifyAlert,
+        success: NotifyAlert,
+        info: NotifyAlert,
+      }}
+    >
+      {children}
+    </SnackbarProvider>
+  )
+}
