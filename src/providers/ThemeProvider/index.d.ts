06cab7c73764251e1c437bcafadd498c952820a6
Luewngier <g217881111@gmail.com>
2023-06-19 16:04:18 +0800

    build: create ThemeProvider in providers

---
+++ src/providers/ThemeProvider/index.d.ts
@@ -0,0 +1,62 @@
+import { CSSProperties } from 'react'
+
+export interface PaletteOptionType {
+  light1?: string
+  light2?: string
+  light3?: string
+  light4?: string
+  dark1?: string
+  dark2?: string
+  dark3?: string
+  dark4?: string
+}
+
+export interface GrayLayer {
+  '00': string
+  '21': string
+  '42': string
+  '61': string
+  '75': string
+  '9e': string
+  bd: string
+  e0: string
+  ee: string
+  f5: string
+  fa: string
+  ff: string
+}
+
+// 可參考 https://mui.com/material-ui/customization/palette/#palette-colors
+declare module '@mui/material/styles' {
+  // Typography
+
+  interface TypographyVariants {
+    chip: CSSProperties
+  }
+  interface TypographyVariantsOptions {
+    chip?: CSSProperties
+  }
+
+  // Palette
+  interface Palette {
+    gray: GrayLayer
+  }
+  interface PaletteOptions {
+    gray: GrayLayer
+  }
+  interface Theme {
+    fadeShadows: ['none', string, string, string, string]
+  }
+  interface ThemeOptions {
+    fadeShadows?: ['none', string, string, string, string]
+  }
+
+  interface PaletteColor extends PaletteOptionType {}
+  interface SimplePaletteColorOptions extends PaletteOptionType {}
+}
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    chip: true
+  }
+}
